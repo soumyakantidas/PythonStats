73aa6829f23cc2955cd1839748a19d82a1e1cad4
Soumyakanti Das <soumyakantidas007@gmail.com>
2017-05-15 17:49:27 +0530

    Office, 15-May-2017

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import Stats
 import matplotlib.pyplot as plt
 
+colors = ['#f7fbff', '#deebf7', '#c6dbef', '#9ecae1', '#6baed6', '#4292c6', '#2171b5', '#08519c', '#08306b']
+
 h = Stats.Hist(Stats.Counter([1, 1, 2, 3, 5, 6, 6, 3]))
 
 values = h.values()
@@ -13,5 +15,5 @@ for key, value in h.items():
     print(key, value)
 
 xs, ys = zip(*h.items())
-plt.bar(xs, ys)
+plt.bar(xs, ys, color=colors[8])
 plt.show()
